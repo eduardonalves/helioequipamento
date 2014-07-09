84ffa238d9024e51f0230a02e28c3f4bb416eaa1
Marketing <marketing@marketing-desktop.(none)>
2014-07-09 11:15:55 -0300

    Alteracoes

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'equipamento');
 define('DB_USER', 'root');
 
 /** Senha do banco de dados MySQL */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'vento');
 
 /** nome do host do MySQL */
 define('DB_HOST', 'localhost');
